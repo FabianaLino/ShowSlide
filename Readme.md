f5e53c2b890b862167d509e46c38e26d5ab0ede0
FabianaLino <fabianalino@icloud.com>
2023-08-15 18:11:09 -0300

    Atualizando cod. imagens e vídeo

--- Readme.md
+++ Readme.md
@@ -1,1 +1,36 @@
-Criando um slide de imagens ensaio joias.
+# Slide de fotos
+
+##### Neste projeto foi desenvolvido um slide de fotos para um catálago. Utilizando tecnologias como HTML, CSS, JavaScript e Hooks do React, como useState, useEffect, useRef, também usei a Lib framer-motion para dar movimento aos slides ao recarregar a página.
+
+##### Segue abaixo layout da aplicação:
+
+<video src="slide/src/images/videolayout.mp4" controls title="Title"></video>
+
+---
+
+## Projeto criado por:
+
+<div>
+<img height="150em" src="https://github-readme-stats.vercel.app/api?username=FabianaLino&show_icons=true&theme=radical">
+
+<img height="150em" src="https://github-readme-stats.vercel.app/api/top-langs/?username=FabianaLino&compact_progress=true">
+</div>
+
+---
+
+## Contato
+
+<a href="https://www.linkedin.com/in/fabiana-lino/">
+
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-original.svg" align="center" height="50" width="60">
+</a>
+
+## Linguagens
+
+<div>
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" align="center" height="50" width="60">
+
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" align="center" height="50" width="60">
+
+<img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" align="center" height="50" width="60">
+</div>
--- slide/src/App.js
+++ slide/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import { motion } from "framer-motion";
+
+import image1 from "../src/images/image1.jpg";
+import image2 from "../src/images/image2.jpg";
+import image3 from "../src/images/image3.jpg";
+import image4 from "../src/images/image4.jpg";
+import image5 from "../src/images/image5.jpg";
+import image6 from "../src/images/image6.jpg";
+import image7 from "../src/images/image7.jpg";
+
+const images = [image1, image2, image3, image4, image5, image6, image7];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const carousel = useRef();
+    const [width, setWidth] = useState(0);
+
+    useEffect(() => {
+        console.log(
+            carousel.current?.scrollWidth,
+            carousel.current?.offsetWidth
+        );
+        setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth);
+    }, []);
+
+    return (
+        <div className="App">
+            <motion.div
+                ref={carousel}
+                className="carousel"
+                whileTap={{ cursor: "grabbing" }}
+            >
+                <motion.div
+                    className="inner"
+                    drag="x"
+                    dragConstraints={{ right: 0, left: -width }}
+                    initial={{ x: 200 }}
+                    animate={{ x: 0 }}
+                    transition={{ duration: 1.5 }}
+                >
+                    {images.map((image) => (
+                        <motion.div className="item" key={image}>
+                            <img src={image} alt="Fotos para slide"></img>
+                        </motion.div>
+                    ))}
+                </motion.div>
+            </motion.div>
+        </div>
+    );
 }
 
 export default App;
